022bebff3c8ed92ea00ae9dd5d35453a7d3789ae
sean ariel <seanariel@hotmail.be>
2016-08-16 17:50:05 +0200

    finalize progress bar

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,9 +7,12 @@ $(document).ready(function(){
 function updateBarProgress(){
   if ($('#search_term').val() == '' &&  $('#city').val() == '') {
     $('.progress-bar').text('0%');
+    $('.progress-bar').css('width', '0%');
   } else if ($('#search_term').val() != '' &&  $('#city').val() != ''){
     $('.progress-bar').text('100%');
+    $('.progress-bar').css('width', '100%');
   } else if ($('#search_term').val() != '' ||  $('#city').val() != '') {
     $('.progress-bar').text('50%');
+    $('.progress-bar').css('width', '50%');
   }
 };
