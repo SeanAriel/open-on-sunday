d94ce3ba09351653833811c01680d1fc083b32a3
george kosmopoulos <gkosmo1@hotmail.com>
2016-08-16 17:53:19 +0200

    done with the offer#new offer#create, new.html, offers/_form.html

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,33 @@
+class OffersController < ApplicationController
+
+  before_action :authenticate_user!, only: [:create]
+#  accept_nested_attributes_for :availabilities, :allow_destroy => true
+
+    def new
+       @offer = Offer.new
+    end
+
+
+   def create
+    # number_to_currency( )
+    @offer = Offer.new(offer_params)
+    @offer.user_id = current_user.id
+    @offer.sold = false
+
+    @offer.save!
+    redirect_to 'home'
+
+   end
+
+
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+     def set_offer
+        @offer = Offer.find(params[:id])
+     end
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def offer_params
+        params.require(:offer).permit(:name, :price, :description, :category_id)
+      end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816093558) do
+ActiveRecord::Schema.define(version: 20160816140159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160816093558) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "city"
     t.index ["category_id"], name: "index_offers_on_category_id", using: :btree
     t.index ["user_id"], name: "index_offers_on_user_id", using: :btree
   end
