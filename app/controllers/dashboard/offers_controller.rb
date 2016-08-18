2fe2ed8752598854941b186e2fc2845caa4876c0
sean ariel <seanariel@hotmail.be>
2016-08-18 17:53:00 +0200

    complete meeting back end

--- app/controllers/dashboard/offers_controller.rb
+++ app/controllers/dashboard/offers_controller.rb
@@ -3,5 +3,19 @@ class Dashboard::OffersController < ApplicationController
     @offers = current_user.offers
     @user = current_user
     @reviews = current_user.reviews
+    @owner_meetings = current_user.meetings
+
+    @bookings = set_bookings(@offers)
+
+  end
+
+  private
+
+  def set_bookings(offers)
+    booking = []
+    offers.each do |offer|
+    booking << offer.meetings
+    end
+    return booking.flatten
   end
 end
