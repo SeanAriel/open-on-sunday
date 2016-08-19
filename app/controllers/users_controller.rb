f4a1988624e19363b7a232b207530c15e2b0ddbc
george kosmopoulos <gkosmo1@hotmail.com>
2016-08-19 17:27:15 +0200

    lastened

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,39 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @offers = @user.offers
+    @owner_meetings = @user.meetings
+
+    @messtousers = @user.messtousers
+    @bookings = set_bookings(@offers)
+
+    #@reviews_give = @user.reviews #review you gave to other ppl
+
+    @buy_review_received = @user.meetings.map do |meeting|
+      meeting.reviews.where.not(user_id: @user.id)
+    end
+
+    @sell_reviews = []
+    @user.offers.each do |offer|
+      offer.meetings.each do |meeting|
+      @sell_reviews << meeting.reviews.where.not(user_id: @user.id)
+      end
+    end
+    # @user.offers.each do |offer|
+    #   offers.meetings.each do |meeting|
+    #     @sell_reviews << meeting.reviews.where.not(user_id: @user.id)
+    #   end
+    # end
+
   end
+
+    private
+
+    def set_bookings(offers)
+      booking = []
+      offers.each do |offer|
+      booking << offer.meetings
+      end
+      return booking.flatten
+    end
 end
